01d7e86688ec50ac0e9d6b01c4427573234bd7f8
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-11 12:53:49 +0000

    Function that sums up numbers in a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" List of float numbers sum """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Function that sums up numbers in a list """
+    return sum(input_list)
