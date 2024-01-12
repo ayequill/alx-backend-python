0bf88ba6007811d7ec27b00b73f310da73329f22
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-12 00:54:29 +0000

    Type Annotation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ List of float numbers sum """
-
 from typing import TypeVar, Mapping, Any, Union
 
 T = TypeVar('T')
@@ -9,7 +8,7 @@ T = TypeVar('T')
 def safely_get_value(
         dct: Mapping, key: Any, default: Union[T, None] = None) \
         -> Union[Any, T]:
-    """ """
+    """ Doc """
     if key in dct:
         return dct[key]
     else:
