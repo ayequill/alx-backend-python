2ec63eb5f384499ea6da984c5e7d57a155312dc6
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-16 22:49:52 +0000

    Fix return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Doc for module """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Doc for function """
     for _ in range(10):
         await asyncio.sleep(1)
