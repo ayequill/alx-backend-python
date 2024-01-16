f8121e0722f93e3120b03389e38183f86dd316d7
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-16 22:40:05 +0000

    Added async generator function

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Doc for module """
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator:
+    """ Doc for function """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
