b1498356a48313c2a3294f9779bc9118ed82ddb1
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-05 21:55:31 +0000

    Added test for org method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,3 +20,17 @@ class TestGithubOrgClient(TestCase):
         instance = GithubOrgClient(org_name)
         instance.org()
         mock.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+    # @patch('client.GithubOrgClient.org')
+    def test_public_repos_url(self):
+        """ Test public_repos_url method """
+        with patch.object(
+                GithubOrgClient,
+                attribute='org',
+                new_callable=Mock,
+                return_value={"repos_url": "https://api.github.com/orgs/google"}
+        ) as mock_org:
+            instance = GithubOrgClient("google")
+            res = instance._public_repos_url
+        self.assertEqual(res, "https://api.github.com/orgs/google")
+        mock_org.assert_called_once()
